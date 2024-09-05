474886cc90e108a548613116fb18034ac4613625
Deepak Kumar <121055671+Deepak19661901@users.noreply.github.com>
2024-09-06 01:03:46 +0530

    Update Application.jsx

--- Frontend/src/components/Application/Application.jsx
+++ Frontend/src/components/Application/Application.jsx
@@ -34,7 +34,7 @@ const Application = () => {
     formData.append("jobId", id);
   
     try {
-      const { data } = await axios.post("http://localhost:4000/api/v2/application/post", formData, {
+      const { data } = await axios.post("https://job-portal-ayfi.onrender.com/api/v2/application/post", formData, {
         withCredentials: true,
         headers: {
           "Content-Type": "multipart/form-data"
