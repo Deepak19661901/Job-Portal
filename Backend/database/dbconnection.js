095beec3d78c1396233ba1458733afcda7cb2e23
deepak kumar <deepakkumarnieit@gmail.com>
2024-09-06 00:55:15 +0530

    mogodb atlas add

--- Backend/database/dbconnection.js
+++ Backend/database/dbconnection.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 
  const dbConnection = () =>{
-  mongoose.connect(process.env.MONGO_URI,{
+  mongoose.connect('mongodb+srv://deepakkumarnieit:tp31gYq56QFx0CLF@cluster0.8wiir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
     dbName:'CAREER_SAFALTA'
   }).then(()=>{
     console.log("dataBase connection succesfully")
