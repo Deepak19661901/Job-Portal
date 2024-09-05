91637451460a09dc91427c316c805526665382fa
Deepak Kumar <121055671+Deepak19661901@users.noreply.github.com>
2024-09-06 01:07:07 +0530

    Update Navbar.jsx

--- Frontend/src/components/LayOut/Navbar.jsx
+++ Frontend/src/components/LayOut/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     try {
-      const response = await axios.get('http://localhost:4000/api/v2/user/logout', {
+      const response = await axios.get('https://job-portal-ayfi.onrender.com/api/v2/user/logout', {
         withCredentials: true,
       });
       toast.success(response.data.message);
